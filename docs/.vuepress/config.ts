353330b52b850c598cd5d543773251580462cc8f
06linux <linux_wuliqiang@qq.com>
2022-04-28 20:19:14 +0800

    菜单测试

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,4 +1,6 @@
 import { defineUserConfig } from 'vuepress'
+const { defaultTheme } = require('@vuepress/theme-default')
+
 
 // const baseUrlOnServer = '/06linux_wiki/';   // 正式环境路径
 // const base = process.env.NODE_ENV == 'development'? '/': baseUrlOnServer;
@@ -9,6 +11,87 @@ const base = '/';
 export default defineUserConfig({
   base: base,
   lang: 'zh-CN',
-  title: '06Linux 站点',
-  description: '这是我的第一个 VuePress 站点',
+  title: '06Linux',
+  description: '06linux 的个人 wiki，欢迎光临',
+
+  theme: defaultTheme({
+    logo: '/images/hero.png',
+    repo: 'https://github.com/06linux/06linux_wiki',
+
+
+    // 不同子路径下的页面会使用不同的侧边栏
+    sidebar: {
+      '/': [
+        {
+          text: '首页',
+          // collapsible: true,
+          children: ['/test/test1.md', '/test/test2.md'],
+        },
+        {
+          text: '测试页面',
+          // collapsible: true,
+          children: ['/test/test1.md', '/test/test2.md'],
+        },
+        {
+          text: '测试页面',
+          collapsible: true,
+          children: ['/testxx/vite.md', '/testxx/webpack.md'],
+        },
+
+        // SidebarItem
+        {
+          text: 'Foo',
+          link: '/foo/',
+          children: [
+            // SidebarItem
+            {
+              text: 'github',
+              link: 'https://github.com',
+              children: [],
+            },
+            {
+              text: 'baidu',
+              link: 'https://baidu.com',
+              children: [],
+            },
+          ],
+        },
+        // 字符串 - 页面文件路径
+        '/bar/README.md',
+      ],
+      '/test/': [
+        {
+          text: 'VuePress Reference',
+          collapsible: true,
+          children: ['/reference/cli.md', '/reference/config.md'],
+        },
+        {
+          text: 'Bundlers Reference',
+          collapsible: true,
+          children: ['/reference/bundler/vite.md', '/reference/bundler/webpack.md'],
+        },
+      ],
+    },
+
+
+
+
+
+    navbar: [
+      // NavbarItem
+      {
+        text: '测试',
+        link: '/test/',
+      },
+      // NavbarGroup
+      {
+        text: '功能测试',
+        children: ['/test/test1.md', '/test/test2.md'],
+      },
+      {
+        text: '关于',
+        link: '/about.md',
+      },
+    ],
+  }),
 })
---
+++ docs/about.md
@@ -0,0 +1,23 @@
+---
+lang: zh-CN
+title: 关于页面
+description: 我的个人简介
+---
+
+## 站点说明
+
+一直想做一个自己的 wiki 网站，将一些资料整理归纳收集在一起
+
+# 作者
+
++ 80 后中年老大叔一枚
++ 爱好编程
+
+
+## 联系方式
++ QQ: 605606385
++ Email: linux_wuliqiang@qq.com
+
+
+## 感谢
++ 特别感谢 [vuepress](https://v2.vuepress.vuejs.org/zh/) 
